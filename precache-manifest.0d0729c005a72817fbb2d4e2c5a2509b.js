63c250df6ccea5fa3ba235498eca9eb3620a8026
Harshitk002 <hk7017438939@gmail.com>
2021-12-28 13:21:41 +0530

    Updates

---
+++ precache-manifest.0d0729c005a72817fbb2d4e2c5a2509b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43ee6b6d2dac005320a85dae9cb7b2d",
+    "url": "/Keeper_App/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Keeper_App/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "a8b02281243b823b8c95",
+    "url": "/Keeper_App/static/js/main.1a925adf.chunk.js"
+  },
+  {
+    "revision": "08392bd83b0bd60e8cf9",
+    "url": "/Keeper_App/static/js/runtime-main.12534e6e.js"
+  }
+]);
